817b811a0082c285c2880e754d981f1da4d2137b
liyc0927 <544151202@qq.com>
2018-04-19 02:53:28 +0800

    test 0001#

--- app/src/main/java/com/cccisi/privacycollector/lyc/LycActivity.java
+++ app/src/main/java/com/cccisi/privacycollector/lyc/LycActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.cccisi.privacycollector.R;
+
 /**
  * Created by cccis on 2018/4/12.
  * LYC负责
@@ -14,5 +16,6 @@ public class LycActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyc_layout);
     }
 }
